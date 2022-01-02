9d7273869c7308a86327efaf59c194670fba363c
Laurie <elaurllc@gmail.com>
2022-01-02 15:32:52 -0500

    Refactoring

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -6,46 +6,24 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    Animator anim;
-    NavMeshAgent playerNav;
     Ray lastRay;
     float speed;
-    void Start()
-    {
-       playerNav =  GetComponent<NavMeshAgent>();
-        anim = GetComponentInChildren<Animator>();
-    }
-
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            MoveToCursor();
-        }
+
         UpdateAnimator();
     }
 
-    private void MoveToCursor()
+    public void MoveTo(Vector3 destination)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-         if (hasHit)
-        {
-            MoveTo(hit);
-        }
-    }
-
-    private void MoveTo(RaycastHit hit)
-    {
-        playerNav.destination = hit.point;
+        GetComponent<NavMeshAgent>().destination = destination;
     }
 
     void UpdateAnimator()
     {
-        var vel = playerNav.velocity;
+        var vel = GetComponent<NavMeshAgent>().velocity;
         var localVelocity = transform.InverseTransformDirection(vel);
         float speed = localVelocity.z;
-        anim.SetFloat("forwardSpeed", speed);
+        GetComponentInChildren<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
