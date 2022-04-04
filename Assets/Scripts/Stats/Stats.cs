39a579b71514a0d72ba229ee920a3a5f9f6adccc
Avocado <avocadofiregaming@gmail.com>
2022-04-04 14:25:22 -0400

    Finished Character Stats #1

---
+++ Assets/Scripts/Stats/Stats.cs
@@ -0,0 +1,12 @@
+//to be an enum for things like health and experience reward
+namespace RPG.Stats
+{
+    enum Stat
+    {
+        Health,
+        ExperienceReward
+                
+    }
+
+}
+
